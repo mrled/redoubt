8812fdbac9a6e68418dedfe786a70b608996f1bd
Micah R Ledbetter <me@micahrl.com>
2023-05-22 12:27:07 -0500

    Initial Commit

---
+++ MindFort/MindFortApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindFortApp.swift
+//  MindFort
+//
+//  Created by Micah R Ledbetter on 2023-05-22.
+//
+
+import SwiftUI
+
+@main
+struct MindFortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
