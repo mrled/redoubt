7595ba60000416446af61a23c22a3f7cacd03de2
Micah R Ledbetter <me@micahrl.com>
2023-05-31 17:11:37 -0500

    Remove DayIntervalPicker.txt, ScheduleSheet, comment
    All added in previous checkpoint commit

--- MindFort/Views/SecretListView.swift
+++ MindFort/Views/SecretListView.swift
@@ -14,7 +14,6 @@ struct SecretListView: View {
     @State private var isPresentingAddSheet = false
     @State private var isPresentingSettingsSheet = false
     @State private var isPresentingAcknowledgementsSheet = false
-    @State private var isPresentingScheduleSheet = false
     @State private var isPresentingControlPanelSheet = false
     @State private var newSecretName = ""
     @State private var newSecretValue = ""
@@ -38,9 +37,6 @@ struct SecretListView: View {
                 ToolbarItemGroup(placement: .navigationBarLeading) {
                     Button(action: { isPresentingSettingsSheet = true }) {
                         Image(systemName: "gear")
-                    }
-                    Button(action: { isPresentingScheduleSheet = true }) {
-                        Image(systemName: "calendar")
                     }
                     Button(action: { isPresentingAcknowledgementsSheet = true }) {
                         Image(systemName: "info.square")
@@ -73,9 +69,6 @@ struct SecretListView: View {
             .sheet(isPresented: $isPresentingAcknowledgementsSheet) {
                 AboutSheet()
             }
-            .sheet(isPresented: $isPresentingScheduleSheet) {
-                ScheduleSheet()
-            }
             .sheet(isPresented: $isPresentingControlPanelSheet) {
                 ControlPanelSheet()
             }
@@ -89,26 +82,6 @@ struct SecretListView: View {
     func removeSecrets(at offsets: IndexSet) {
         viewModel.secrets.remove(atOffsets: offsets)
     }
-
-//    func registerNotifications() {
-////        let today = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: Date())
-//        var dateComponents2359 = DateComponents()
-//        dateComponents2359.hour = 23
-//        dateComponents2359.minute = 59
-////        dateComponents.year = today.year
-////        dateComponents.month = today.month
-////        dateComponents.day = today.day
-//
-//        let trigger2359 = UNCalendarNotificationTrigger(dateMatching: dateComponents2359, repeats: true)
-//        NotificationManager.shared.registerNotification(title: "Password ritual", body: "Time to perform a passphrase ritual 🙏", identifier: "1159pmNotification", trigger: trigger2359)
-//        
-//        var dateComponents1800 = DateComponents()
-//        dateComponents1800.hour = 18
-//        dateComponents1800.minute = 0
-//        let trigger1800 = UNCalendarNotificationTrigger(dateMatching: dateComponents1800, repeats: true)
-//        NotificationManager.shared.registerNotification(title: "Password ritual", body: "Time to perform a passphrase ritual 🙏", identifier: "1800Notification", trigger: trigger1800)
-//    }
-
 }
 
 struct SecretListView_Previews: PreviewProvider {
