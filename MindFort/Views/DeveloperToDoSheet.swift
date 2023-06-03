99c392434365ea60806166cafd339da2d70a3987
Micah R Ledbetter <me@micahrl.com>
2023-06-02 22:09:47 -0500

    todo

--- MindFort/Views/DeveloperToDoSheet.swift
+++ MindFort/Views/DeveloperToDoSheet.swift
@@ -17,9 +17,7 @@ struct DeveloperToDoSheet: View {
             List {
                 Section(header: Text("Add features")) {
                     RowItemWithIcon(title: "Spaced repetition schedule editor", systemImageName: "clock")
-                    RowItemWithIcon(title: "Option to disable HAXX0R C0D3", systemImageName: "laptopcomputer")
                     RowItemWithIcon(title: "Try color schemes", systemImageName: "sparkles")
-                    RowItemWithIcon(title: "Are you sure? for deletes", systemImageName: "questionmark.circle")
                     RowItemWithIcon(title: "Undo for deletes", systemImageName: "arrow.uturn.backward")
                     // https://developer.apple.com/documentation/uikit/protecting_the_user_s_privacy/encrypting_your_app_s_files
                     // xcode project -> create capability -> Data Protection
@@ -28,6 +26,8 @@ struct DeveloperToDoSheet: View {
                     RowItemWithIcon(title: "Add geofencing ability", systemImageName: "mappin.and.ellipse")
                     RowItemWithIcon(title: "Allow editing name of existing entries", systemImageName: "square.and.pencil")
                     RowItemWithIcon(title: "Use some good key derivation function for password storage", systemImageName: "lock")
+                    RowItemWithIcon(title: "Add single place for AppStorage defaults and keys", systemImageName: "externaldrive")
+                    RowItemWithIcon(title: "Keyboard shortcuts, at least enter to create, maybe escape to go back", systemImageName: "return")
                 }
                 Section(header: Text("Fix bugs")) {
                     RowItemWithIcon(title: "Tapping on notification should launch quiz", systemImageName: "ladybug")
@@ -37,6 +37,8 @@ struct DeveloperToDoSheet: View {
                     RowItemWithIcon(title: "Implement notifications", systemImageName: "bell")
                     RowItemWithIcon(title: "Show all of H4XX0R C0D3 w/ keyboard enabled", systemImageName: "ladybug")
                     RowItemWithIcon(title: "Add previous/next buttons to detail view", systemImageName: "arrow.right")
+                    RowItemWithIcon(title: "Option to disable HAXX0R C0D3", systemImageName: "laptopcomputer")
+                    RowItemWithIcon(title: "Are you sure? for deletes", systemImageName: "questionmark.circle")
                 }
             }
         }
