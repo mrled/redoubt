4c6ecd3f5b2d6a0553968ad682eed9b9d77b1773
Micah R Ledbetter <me@micahrl.com>
2023-06-04 14:36:15 -0500

    Take all the animation stuff out of ShimmeringSystemImage
    I think it was causing performance problems, and it wasn't working anyway

--- MindFort/UtilityViews/ShimmeringSystemImage.swift
+++ MindFort/UtilityViews/ShimmeringSystemImage.swift
@@ -9,41 +9,18 @@ import SwiftUI
 
 struct ShimmeringSystemImage: View {
     var systemName: String
-    @State private var isShimmering = false
-//    @State private var gradientColors: [Color] = [Color.purple, Color.red.opacity(0.5), Color.blue]
     @State private var gradientColors: [Color] = [Color.red, Color.green, Color.blue]
 
-//    var body: some View {
-//        LinearGradient(
-//            gradient: Gradient(colors: gradientColors),
-//            startPoint: .leading,
-//            endPoint: .trailing
-//        )
-//        .mask(
-//            Image(systemName: systemName)
-//                .foregroundColor(.white)
-//        )
-//        .onAppear {
-//            withAnimation(.linear(duration: 3)) {
-//                isShimmering = true
-//            }
-//        }
-//    }
-
     var body: some View {
-        ZStack {
-            withAnimation(.linear(duration: 3)) {
-                LinearGradient(
-                    gradient: Gradient(colors: gradientColors),
-                    startPoint: .leading,
-                    endPoint: .trailing
-                )
-                .mask(
-                    Image(systemName: systemName)
-                        .foregroundColor(.white)
-                )
-            }
-        }
+        LinearGradient(
+            gradient: Gradient(colors: gradientColors),
+            startPoint: .leading,
+            endPoint: .trailing
+        )
+        .mask(
+            Image(systemName: systemName)
+                .foregroundColor(.white)
+        )
     }
 }
 
