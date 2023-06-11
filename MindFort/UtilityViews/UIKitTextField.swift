7c27456ec2d83390bfe144b6209cef7d8ea9b2cf
Micah R Ledbetter <me@micahrl.com>
2023-06-11 15:13:43 -0500

    Add UIKitTextField experiment

---
+++ MindFort/UtilityViews/UIKitTextField.swift
@@ -0,0 +1,73 @@
+//
+//  MFTextField.swift
+//  MindFort
+//
+//  Created by Micah R Ledbetter on 2023-06-11.
+//
+
+import SwiftUI
+
+
+/// A SwiftUI wrapper around UIKIt TextField
+/// ChatGPT says this (with isSecureTextEntry) may be more performant than SwiftUI's SecureField.
+struct UIKitTextField: UIViewRepresentable {
+    var title: String
+    var text: Binding<String>
+    var isSecureTextEntry: Bool
+    
+    init(_ title: String, text: Binding<String>, isSecureTextEntry: Bool = false) {
+        self.title = title
+        self.text = text
+        self.isSecureTextEntry = isSecureTextEntry
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        textField.isSecureTextEntry = isSecureTextEntry
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text.wrappedValue
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: UIKitTextField
+
+        init(_ textField: UIKitTextField) {
+            self.parent = textField
+        }
+
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            if let currentText = textField.text,
+               let range = Range(range, in: currentText) {
+                parent.text.wrappedValue = currentText.replacingCharacters(in: range, with: string)
+            }
+            return true
+        }
+    }
+}
+
+
+struct MFTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        @State var text = "Example Text"
+        Group {
+            VStack {
+                UIKitTextField("Field title...", text: $text)
+            }
+            .padding()
+            .previewDisplayName("Regular text")
+            VStack {
+                UIKitTextField("Field title...", text: $text, isSecureTextEntry: true)
+            }
+            .padding()
+            .previewDisplayName("Secure text")
+        }
+    }
+}
