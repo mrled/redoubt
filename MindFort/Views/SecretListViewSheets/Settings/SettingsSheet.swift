1da5977a840273685b1fd2eed0ba6233a8c9efe5
Micah R Ledbetter <me@micahrl.com>
2023-06-17 16:32:07 -0500

    Handle biometric lockout

--- MindFort/Views/SecretListViewSheets/Settings/SettingsSheet.swift
+++ MindFort/Views/SecretListViewSheets/Settings/SettingsSheet.swift
@@ -23,6 +23,15 @@ enum VisualizationMode: String, Codable, CaseIterable, Identifiable {
 }
 
 
+/// Possible alerts the Settings sheet can show
+enum SettingsAlertType {
+    case none
+    case biometricsUnavailable
+    case biometricsFailed
+    case biometricsLockedOut
+}
+
+
 struct RegularIntervalScheduleControls: View {
     @EnvironmentObject var notificationsModel: NotificationsViewModel
     
@@ -157,8 +166,8 @@ struct SettingsControls: View {
     @Binding var enableEasterEggs: Bool
     @Binding var visualizationMode: VisualizationMode
     @Binding var demoMode: Bool
-    @State private var showBiometricsUnavailableAlert = false
-    @State private var showBiometricsFailedAlert = false
+    @State private var showAlert = false
+    @State private var alertType: SettingsAlertType = .none
     var body: some View {
         Section("Settings") {
             // Not sure if it's the Toggles or what, but the spacing doesn't match RowItemWithIcon.
@@ -208,41 +217,73 @@ struct SettingsControls: View {
                 }
             }
         }
-        .alert(isPresented: $showBiometricsUnavailableAlert) {
-            Alert(
-                title: Text("Biometrics unavailable"),
-                message: Text("Please set up Face ID or Touch ID in your device settings"),
-                dismissButton: .default(Text("OK"))
-            )
-        }
-        .alert(isPresented: $showBiometricsFailedAlert) {
-            Alert(
-                title: Text("Biometrics failed"),
-                message: Text("Could not authenticate with Face ID or Touch ID"),
-                dismissButton: .default(Text("OK"))
-            )
+        .alert(isPresented: $showAlert) {
+            switch alertType {
+            case .none:
+                return Alert(
+                    title: Text("Something went wrong"),
+                    message: Text("This should never happen"),
+                    dismissButton: .default(Text("OK"))
+                )
+            case .biometricsUnavailable:
+                return Alert(
+                    title: Text("Biometrics unavailable"),
+                    message: Text("Please set up Face ID or Touch ID in your device settings"),
+                    dismissButton: .default(Text("OK"))
+                )
+            case .biometricsFailed:
+                return Alert (
+                    title: Text("Biometrics failed"),
+                    message: Text("Could not authenticate with Face ID or Touch ID"),
+                    dismissButton: .default(Text("OK"))
+                )
+            case .biometricsLockedOut:
+                return Alert (
+                    title: Text("Biometrics locked out"),
+                    message: Text("Too many Face ID or Touch ID failures"),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
         }
     }
+    
+    
     
     func toggleDemoModeWithAuthentication() {
         let context = LAContext()
         var error: NSError?
 
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "Please authenticate to toggle Demo Mode"
+            let reason = "Please authenticate to change the setting."
 
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-
                 DispatchQueue.main.async {
                     if success {
-                        demoMode.toggle()
+                        self.demoMode.toggle()
                     } else {
-                        self.showBiometricsFailedAlert = true
+                        self.alertType = .biometricsFailed
+                        self.showAlert = true
                     }
                 }
             }
         } else {
-            self.showBiometricsUnavailableAlert = true
+            if let laError = error as? LAError, laError.code == .biometryLockout {
+                let reason = "You've attempted too many times! Enter your passcode to enable biometrics."
+                
+                context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, _ in
+                    DispatchQueue.main.async {
+                        if success {
+                            self.demoMode.toggle()
+                        } else {
+                            self.alertType = .biometricsFailed
+                            self.showAlert = true
+                        }
+                    }
+                }
+            } else {
+                self.alertType = .biometricsUnavailable
+                self.showAlert = true
+            }
         }
     }
 }
