7bf0f28787340c47ac44505c4a3001ef23992343
Micah R Ledbetter <me@micahrl.com>
2025-06-12 21:47:18 -0500

    Use modern API for md5

--- MindFort/Models/Hashes.swift
+++ MindFort/Models/Hashes.swift
@@ -27,26 +27,12 @@ func sha512(string: String) throws -> Data  {
 }
 
 
-/// NOTE: we're just using this for UUIDs! Nothing else!
-func md5Hash(of data: Data) -> Data {
-    var hash = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-    data.withUnsafeBytes { dataBytes in
-        hash.withUnsafeMutableBytes { hashBytes in
-            _ = CC_MD5(dataBytes.baseAddress, CC_LONG(data.count), hashBytes.bindMemory(to: UInt8.self).baseAddress)
-        }
-    }
-    return hash
-}
-
-
 /// This is the easiest way to get a Swift UUID from arbitrary data.
 /// It's weak against collision attacks because lol md5 of course.
 /// It's also not a valid UUID per the UUID specification.
 /// What we want to use it for is the Identifiable protocol, and only that, so we're not worried about it.
 func md5UUID(data: Data) -> UUID {
-    let hashedData = md5Hash(of: data)
+    let hashedData = Data(Insecure.MD5.hash(data: data))
     let uuid = UUID(uuid: hashedData.withUnsafeBytes { $0.load(as: uuid_t.self) })
     return uuid
 }
-
-
