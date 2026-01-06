9fe58b53442067cf8bfa632bebd39861543b635b
Micah R Ledbetter <me@micahrl.com>
2026-01-06 14:14:05 -0600

    Refactor SettingsControls to its own file

--- Redoubt/Views/Screens/SecretListViewSheets/SettingsSheet.swift
+++ Redoubt/Views/Screens/SecretListViewSheets/SettingsSheet.swift
@@ -326,63 +326,6 @@ struct ScheduleControls: View {
 }
 
 
-struct SettingsControls: View {
-    @Binding var showOnboarding: Bool
-    @Binding var showDeveloperOptions: Bool
-    @Binding var enableEasterEggs: Bool
-    @Binding var visualizationMode: VisualizationMode
-    @Binding var demoMode: Bool
-    var body: some View {
-        Section("Settings") {
-            // Not sure if it's the Toggles or what, but the spacing doesn't match RowItemWithIcon.
-            // Just make them all HStack{Image, Text} and they look the same.
-            Toggle(isOn: $showOnboarding) {
-                // is a RowItemWithIcon except the Icon is a ShimmeringSystemImage
-                HStack {
-                    ShimmeringSystemImage(systemName: "play")
-                        .frame(width: 32, height: 32)
-                    Text("Show the onboarding button")
-                }
-            }
-            Toggle(isOn: $enableEasterEggs) {
-                HStack {
-                    Image(systemName: "sparkles")
-                        .frame(width: 32, height: 32)
-                    Text("Enable easter eggs")
-                }
-            }
-            HStack {
-                Image(systemName: "sparkles.tv")
-                    .frame(width: 32, height: 32)
-                Picker(selection: $visualizationMode, label: Text("Visualization type")) {
-                    ForEach(VisualizationMode.allCases) { possibleVizMode in
-                        Text(possibleVizMode.description).tag(possibleVizMode)
-                    }
-                }
-            }
-            Toggle(isOn: $showDeveloperOptions) {
-                HStack {
-                    Image(systemName: "slider.horizontal.3")
-                        .frame(width: 32, height: 32)
-                    Text("Show developer options")
-                }
-            }
-            NavigationLink(destination: DemoModeSheet(isPresentingDemoMode: .constant(false))) {
-                HStack {
-                    Image(systemName: "tv")
-                        .frame(width: 32, height: 32)
-                    if demoMode {
-                        Text("Demo mode (currently enabled)")
-                    } else {
-                        Text("Demo mode (currently disabled)")
-                    }
-                }
-            }
-        }
-    }
-}
-
-
 struct SettingsSheet: View {
     @Binding var notificationsAllowed: Bool
     @AppStorage(MFAStorage.K.enableEasterEggs) var enableEasterEggs: Bool = MFAStorage.D.enableEasterEggs
